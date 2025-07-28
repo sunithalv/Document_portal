4b206a2f94199598a6004c679b0fb0e05b4a2a69
sunithalv <sunithalv05@gmail.com>
2025-07-28 10:14:11 +0530

    Updated files

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="document_portal",
-    author="Sunitha L V",
+    author="Sunny Savita",
     version="0.1",
     packages=find_packages(),
 )
